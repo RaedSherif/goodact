4659f8772aae850b417782803be1e903c40c4c34
Raed Sherif <raedaasherif@gmail.com>
2026-05-24 12:22:02 +0300

    Add logger utility class and create logs directory

---
+++ utils/logger.php
@@ -0,0 +1,31 @@
+<?php
+class Logger
+{
+    private static $instance = null;
+    private $logFile;
+
+    private function __construct()
+    {
+        $this->logFile = __DIR__ . "/../logs/app.log";
+
+        // create the logs folder if it doesn't exist
+        if (!is_dir(dirname($this->logFile))) {
+            mkdir(dirname($this->logFile), 0755, true);
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new Logger();
+        }
+        return self::$instance;
+    }
+
+    public function log($level, $message)
+    {
+        $timestamp = date("Y-m-d H:i:s");
+        $entry = "[$timestamp] [$level] $message" . PHP_EOL;
+        file_put_contents($this->logFile, $entry, FILE_APPEND | LOCK_EX);
+    }
+}
